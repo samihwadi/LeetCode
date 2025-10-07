63118b996392625bbd1cf178616f87c9ba7ed83c
Samih Wadi <123220794+samihwadi@users.noreply.github.com>
2025-10-07 19:28:36 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (39.73%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,9 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = [[]]
-        for num in nums:
-            # For each existing subset, add a new subset including num
-            result += [curr + [num] for curr in result]
+        result = []
+        # Backtracking to find all possible subsets
+        # O(n x 2^n)
+        def backtrack(start, subset):
+            result.append(subset[:])
+            for i in range(start, len(nums)):
+                subset.append(nums[i])
+                backtrack(i + 1, subset)
+                subset.pop()
+        backtrack(0, [])
         return result
 
 
