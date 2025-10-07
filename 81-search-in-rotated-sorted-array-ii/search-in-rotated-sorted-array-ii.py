0873e0c339b7887b3fee003d628119154db4da18
Samih Wadi <123220794+samihwadi@users.noreply.github.com>
2025-10-07 18:45:58 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (95.73%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,13 +1,27 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        nums.sort()
+        # Original solution used sort(), not allowed and solution was O(nlogn) --> X
+        # Modified Binary search --> O(logn)
         left, right = 0, len(nums) - 1
         while left <= right:
             mid = (left + right) // 2
+            # If target is found
             if nums[mid] == target:
                 return True
-            elif nums[mid] < target:
-                left = mid + 1
+            # Shrink window if duplicates
+            if nums[mid] == nums[left] == nums[right]:
+                left += 1
+                right -= 1
+            # If left half is sorted
+            elif nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            # If right half is sorted
             else:
-                right = mid - 1
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else: 
+                    right = mid - 1
         return False
