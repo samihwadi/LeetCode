69c0ba5d2011f955bdffc4b787b3a54e9ba082dd
Samih Wadi <123220794+samihwadi@users.noreply.github.com>
2025-10-07 18:33:39 -0400

    Time: 0 ms (100.00%) | Memory: 18.2 MB (79.00%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,15 +1,13 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
         nums.sort()
-        def binary_search(low, high):
-            if low > high:
-                return False
-            mid = (low + high) // 2
-            if target == nums[mid]:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
                 return True
-            elif target > nums[mid]:
-                return binary_search(mid+1, high)
+            elif nums[mid] < target:
+                left = mid + 1
             else:
-                return binary_search(low, mid-1)
-        
-        return binary_search(0, len(nums)-1)
+                right = mid - 1
+        return False
