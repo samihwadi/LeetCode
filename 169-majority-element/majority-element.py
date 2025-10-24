32def51f18d6d9fbb2a6bb5843f66a7541d04995
Samih Wadi <123220794+samihwadi@users.noreply.github.com>
2025-10-24 15:46:20 -0400

    Time: 7 ms (57.46%) | Memory: 19.4 MB (19.06%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,8 +1,19 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counts = {}
+        # Prefer the Moore Voting Algorithm
+        # Assumes that 1st number is the majority element
+        # Increments count if we encounter the same element, else decrements it
+        # If count reaches 0, that means another element is majority
+        # Return candidate number with count > 0
+
+        count = 0
+        candidate = 0
         for num in nums:
-            counts[num] = counts.get(num, 0) + 1
-        majority = max(counts, key=counts.get)
-        return majority
+            if count == 0:
+                candidate = num
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+        return candidate
         
