8719c953599ee9c0764f3905b8116cb351c5c729
Samih Wadi <123220794+samihwadi@users.noreply.github.com>
2025-10-30 14:57:34 -0400

    Time: 45 ms (78.51%) | Memory: 34.2 MB (67.29%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -4,13 +4,13 @@ class Solution:
         # Hash Set to remove duplicates 
         # Find number which is start of a sequence, i.e, hashset does not contain nums[i] - 1
         # Track length of sequence
-        numSet = set(nums)
+        my_nums = set(nums)
         longest = 0
-        for n in numSet:
+        for n in my_nums:
             # Find start of sequence
-            if n - 1 not in numSet:
+            if n - 1 not in my_nums:
                 length = 0
-                while n + length in numSet:
+                while n + length in my_nums:
                     length += 1
                 longest = max(length, longest)
         return longest
