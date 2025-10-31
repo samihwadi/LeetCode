8ec464a7ba83ba1e76fc674ced7fc14d2c1ef1a5
Samih Wadi <123220794+samihwadi@users.noreply.github.com>
2025-10-31 16:17:32 -0400

    Time: 7 ms (82.39%) | Memory: 18.4 MB (37.53%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,5 +1,23 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        check_str = ''.join(x for x in s if x.isalnum()).lower()
-        return check_str == check_str[::-1]
+        # Cleanup string
+        clean_str = "".join(x for x in s if x.isalnum()).lower()
+        length = len(clean_str)
+        mid = (length // 2)
+        # Have Two Pointers in the middle of the string
+        i, j = 0, 0
+        # If even, start at mid and mid - 1
+        if length % 2 == 0:
+            i = mid
+            j = mid - 1
+        # If odd length, pointers start at same position
+        else:
+            i, j = mid, mid
+        
+        while i < length:
+            if clean_str[i] != clean_str[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
         
